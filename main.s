b0360953dd27145cb9e40e893e60825ce7d1f534
marklihere <mark.li@usu.edu>
2016-09-24 10:51:17 -0600

    Minimal2
    Deleted further commented out lines
    Added comments for delay cycle
    Added our names, cleaned it up

--- main.s
+++ main.s
@@ -1,24 +1,7 @@
-; main.s
-; Runs on any Cortex M processor
-; A very simple first project implementing a random number generator
-; Daniel Valvano
-; May 4, 2012
-
-;  This example accompanies the book
-;  "Embedded Systems: Introduction to Arm Cortex M Microcontrollers",
-;  ISBN: 978-1469998749, Jonathan Valvano, copyright (c) 2012
-;  Section 3.3.10, Program 3.12
-;
-;Copyright 2012 by Jonathan W. Valvano, valvano@mail.utexas.edu
-;   You may use, edit, run or diSTRibute this file
-;   as long as the above copyright notice remains
-;THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
-;OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
-;MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
-;VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
-;OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
-;For more information about my classes, my research, and my books, see
-;http://users.ece.utexas.edu/~valvano/
+; Mark Li and Aaron Yardley
+; Sep 20, 2016
+; ECE 3710 Lab 3
+; 10-bit binary LED counter
 
        THUMB
        AREA    DATA, ALIGN=2
@@ -37,22 +20,10 @@ UNLOCK	EQU 0x4C4F434B  ; Unlock code for GPIO Port F
 PORTF	EQU 0x40025000  ; GPIO Port F
 PORTC	EQU	0x40006000	; GPIO Port C
 PORTE	EQU	0x40024000	; GPIO Port E
-
-BIT0	EQU 0x40006040  ; PC4
-BIT1	EQU 0x40006080  ; PC5
-BIT2	EQU 0x40006100  ; PC6
-BIT3	EQU 0x40006200  ; PC7
-BIT4	EQU 0x40024004  ; PE0
-BIT5	EQU 0x40024008  ; PE1
-BIT6	EQU 0x40024010  ; PE2
-BIT7	EQU 0x40024020  ; PE3
-BIT8	EQU 0x40024040  ; PE4
-BIT9	EQU 0x40024080  ; PE5
 	
 Start  
 ;------------------------------------------------------
 ; Initialize GPIO Ports
-	;MOV32 R0, #0x400FE108 ; Enable GPIO Clock RCGCGPIO
 	LDR R0, =RCGCGPIO ; Enable GPIO Clock RCGCGPIO
 	MOV R1, #0x34    ; Enables GPIO Port C, E, and F
 	STR R1, [R0]     ;
@@ -104,7 +75,7 @@ Reset
 ; Is Start Pushed?  if Y start Running, if N, Reset
 	LDR R0, =GO          ; Read input of Start switch
 	LDR R1, [R0]         ; GPIOF pin 3, the external button
-	CMP R1, #0x0         ; Check if button pushed
+	CMP R1, #0         ; Check if button pushed
 	BEQ Running;
 	B Reset;
 
@@ -117,16 +88,17 @@ Running
 ; Check if reset
 		LDR R0, =RESETB     ; Read input of Reset switch
 		LDR R1, [R0]        ; GPIOF pin0 for Reset switch
-		CMP R1, #0x0        ; reset button pushed?
+		CMP R1, #0        ; reset button pushed?
 		BEQ Reset
 
 ; Check if stop
 		LDR R0, =STOP       ; Addr of Stop switch
 		LDR R1, [R0]        ; Read switch
-		CMP R1, #0x0        ; stop button pushed?
+		CMP R1, #0        ; stop button pushed?
 		BEQ Stopstate       ; Yep
 
 ; enter delay b/c no reset and no stop, so keep counting 
+; 2 Hz means period = 0.5 s period, 0.25 per off/on cycle
 ; Delay loop for remainder of 0.25s
 		MOV32 R0, #0x145855 ; delay counter 1,333,333 cycles 
 delay	SUBS R0, #1    	    ; decrement delay counter
@@ -140,25 +112,27 @@ Stopstate
 ; Check if reset
 		LDR R0, =RESETB     ; Read input of Reset switch
 		LDR R1, [R0]        ; GPIOF pin0 for Reset switch
-		CMP R1, #0x0        ; reset button pushed?
+		CMP R1, #0        ; reset button pushed?
 		BEQ Reset
 
 ; Check if start
 		LDR R0, =GO        ; Read input of Reset switch
 		LDR R1, [R0]       ; GPIOF pin0 for Reset switch
-		CMP R1, #0x0       ; Check if button pushed
+		CMP R1, #0       ; Check if button pushed
 		BEQ Running
 		B Stopstate
 
 ;----------------------
 ; Print Subroutine
+; Displays R4[9:0] on LED bar
+;  Uses ugly global variables, doesn't store
 print
 	PUSH {LR}              ; Store Return
 	MVN R5, R4;            ; Invert counter due to active low LEDs
-	LDR R0, =PORTC         ; GPIOC base address
+	LDR R0, =PORTC
 	LSL R10, R5, #4        ; get lower bits [3:0] of counter into bit position [7:4] of R10
 	STR R10, [R0,#0x3C0]   ; write counter[3:0]  into Port C[7:4]
-	LDR R0, =PORTE         ; GPIOE base address
+	LDR R0, =PORTE
 	LSR R10, R5, #4        ; rotate counter upper 6 bits for LED display into position [5:0]
 	STR R10, [R0,#0x0FC]   ; write upper six bits into LED disply
 	POP {LR}
